67410cc106c280bb368da0cf6dd9c7fe9b0f8866
wu1094366146 <61652427+wu1094366146@users.noreply.github.com>
2023-05-14 20:55:19 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -11,7 +11,11 @@
 #
 
 # Uncomment a feed source
-#sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
+sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
-#sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
+#echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
+echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+echo 'src-git openwrtpackage https://github.com/281677160/openwrt-package' >>feeds.conf.default
+echo 'src-git kenzo https://github.com/V2RaySSR/openwrt-packages' >>feeds.conf.default
+echo 'src-git small https://github.com/V2RaySSR/small' >>feeds.conf.default
